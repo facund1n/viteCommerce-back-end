02e3f79cbd65b4c9c01f2a4c9a8f2ec0228a7408
facund1n <facus16s@gmail.com>
2023-04-27 20:57:11 -0300

    producto model Schema update

--- models/product.js
+++ models/product.js
@@ -3,6 +3,11 @@ const Schema = mongoose.Schema;
 
 const productsSchema = new Schema({
   title: { type: String, required: true, min: 2, max: 80 },
+  description: { type: String, required: true, min: 10, max: 300 },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  stock: { type: Number, required: true },
+  image: { type: String, required: true },
 });
 
 const Products = mongoose.model("products", productsSchema);
