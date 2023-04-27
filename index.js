99b67a1e838ddab99e37411998d64ac3afe1cf36
facund1n <facus16s@gmail.com>
2023-04-27 02:54:23 -0300

    middleware config

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+require("dotenv").config();
+
+// express:
 const express = require("express");
 const app = express();
-const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Back end index upload");
 });
 
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+// mongoose:
+const mongoose = require("mongoose");
+const URI_DB = process.env.URI_DB;
+mongoose
+  .connect(URI_DB)
+  .then(() => console.log("viteCommerce connexion DB ok"))
+  .catch((error) => console.log(error));
+
+// Body Parser:
+app.use(express.json());
+
+// cors:
+var cors = require("cors");
+app.use(cors());
+
+// router:
+const productsRoutes = require("./routes/products");
+app.use("/", productsRoutes);
