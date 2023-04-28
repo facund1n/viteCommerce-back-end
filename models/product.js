26304703f1626b7b65008b29020666c19ac5ad48
facund1n <facus16s@gmail.com>
2023-04-28 02:25:00 -0300

    prod schema update

--- models/product.js
+++ models/product.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 const productsSchema = new Schema({
   title: { type: String, required: true, min: 2, max: 80 },
   description: { type: String, required: true, min: 10, max: 300 },
-  price: { type: Number, required: true },
-  category: { type: String, required: true },
-  stock: { type: Number, required: true },
   image: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true, min: 2, max: 80 },
+  stock: { type: Number, required: true },
 });
 
 const Products = mongoose.model("products", productsSchema);
