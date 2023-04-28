b4232a9bd06d0c9a20ec72b814ed3894dfe12176
facund1n <facus16s@gmail.com>
2023-04-28 02:25:10 -0300

    crud endpoints update

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,62 @@
 const router = require("express").Router();
 const Products = require("../models/product");
 
-router.get("/products", async (req, res) => {
-  try {
-    const allProducts = await Products.find();
-    res.status(200).send(allProducts);
-  } catch (error) {
-    res.status(400).json({ message: error });
-  }
-});
+router
+  .get("/products", async (req, res) => {
+    try {
+      const allProducts = await Products.find();
+      res.status(200).send(allProducts);
+    } catch (error) {
+      res.status(400).json({ message: error });
+    }
+  })
+  .post("/products/new", async (req, res) => {
+    const { body } = req;
+    try {
+      const newProduct = new Products(body);
+      await newProduct.save();
+      res.status(200).json({ newProduct, message: "Uploaded successfully." });
+    } catch (error) {
+      res.status(400).json({ error: true, message: "Upload Error" });
+    }
+  })
+  .get("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const product = await Products.findOne({ _id: id });
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(404).json({ message: error });
+    }
+  })
+  .patch("/products/update/:id", async (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+    try {
+      const editProduct = await Products.findByIdAndUpdate(id, body, {
+        useFindAndModify: false,
+      });
+      res.status(200).json({ editProduct, message: "Edited successfully." });
+    } catch (error) {
+      res.status(404).json({
+        error: true,
+        message: "Failed to edit.",
+      });
+    }
+  })
+  .delete("/articulos/delete/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const delArticle = await Article.findByIdAndDelete({
+        _id: id,
+      });
+      res.status(200).json({ delArticle, message: "Deleted successfully." });
+    } catch (error) {
+      res.status(404).json({
+        error: true,
+        message: "Failed to delete.",
+      });
+    }
+  });
 
 module.exports = router;
