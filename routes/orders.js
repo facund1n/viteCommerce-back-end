d0207b25de7abba380e8be0529e91d5597888079
facund1n <facus16s@gmail.com>
2023-05-09 18:45:02 -0300

    orders endpoints upload

--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Orders = require("../models/orders");
+const Users = require("../models/users");
 
 router
   .get("/orders", async (req, res) => {
@@ -18,6 +19,31 @@ router
       res.status(200).json({ newOrder, message: "Order sent successfully." });
     } catch (error) {
       res.status(404).json({ error: true, message: "Error try again please" });
+    }
+  })
+  .get("/orders/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const userExist = await Orders.findOne({
+      userId: id,
+    });
+
+    if (userExist === null) {
+      return res.status(404).json({ message: "not orders found." });
+    }
+
+    try {
+      if (id === "6450494eb499f437fa44e0ed") {
+        const allOrders = await Orders.find();
+        return res.status(200).json(allOrders);
+      } else {
+        const userOrders = await Orders.find({
+          userId: id,
+        });
+        return res.status(200).json(userOrders);
+      }
+    } catch (error) {
+      return res.status(404).json({ message: "not found" });
     }
   });
 
